fee4ae2ba2befc1be1ef279abcb19dfc727d529b
Steve Gilham <SteveGilham@users.noreply.github.com>
2017-09-10 21:05:11 +0100

    Ensure that the Visual Studio environment is set up once and only once -- now the generated Fake.Bat should be good to go, and we can start extending build.fsx

--- Build/get-nuget.ps1
+++ Build/get-nuget.ps1
@@ -8,10 +8,24 @@ if (-not (Test-Path $nugetPath)) {
 $solutionRoot = Resolve-Path (Join-Path $PSScriptRoot "..")
 & $nugetPath restore $solutionRoot
 $fake = (dir -recurse "$solutionRoot\*ake.exe") | % { $_.FullName } | Select-Object -First 1
+
+## establish vsvars
+$key = "HKLM:\SOFTWARE\Microsoft\VisualStudio\SxS\VS7"
+if (-not (Test-Path $Key))
+{
+    $key = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\VisualStudio\SxS\VS7"  
+}
+$VsKey = get-ItemProperty $key
+$version = "15.0"
+$VsInstallPath = $VsKey.$version
+$VcTools = Join-Path $VsInstallPath "Common7\Tools"
+$BatchFile = Resolve-Path (Join-Path $VcTools VsDevCmd.bat)
+##$path = (($env:path).Split(";") | Select-Object -Unique) -join ";"
 $bat = @"
 @echo off
+IF '"%VS150COMNTOOLS%"' == '""' CALL "$batchFile"
 SET PATH="$($env:path);$(Split-Path -Parent $fake)"
-"$fake" "%1" "%2"
+"$fake" ".\build\build.fsx" "%1"
 exit /b %errorlevel%
 "@
 Set-Content -Value $bat -Path (Join-Path $SolutionRoot "fake.bat")
