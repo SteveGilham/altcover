9dde65dd05912879a275858f024834be28a50dc6
Steve Gilham <SteveGilham@users.noreply.github.com>
2017-09-11 19:03:02 +0100

    Initial build script (modulo we can't dogfood yet)   - will of course evolve.

--- Build/build.fsx
+++ Build/build.fsx
@@ -4,10 +4,13 @@ open System.IO
 
 open Fake
 open Fake.AssemblyInfoFile
+open Fake.Testing
+open Fake.OpenCoverHelper
+open Fake.ReportGeneratorHelper
 
 // The clean target cleans the build and deploy folders
 Target "Clean" (fun _ ->
-    CleanDirs ["./_Binaries/"; "./_Intermediate/"; "./_Generated/"]
+    CleanDirs ["./_Binaries/"; "./_Intermediate/"; "./_Generated/"; "./_Reports"]
 )
 
 Target "SetVersion" (fun _ ->
@@ -22,11 +25,10 @@ Target "SetVersion" (fun _ ->
         [Attribute.Version "0.0.0.0"
          Attribute.FileVersion version]
 
-// [<assembly: InternalsVisibleTo(\"Tests, PublicKey={0}\")>]
     let template ="#light
 namespace Altcover
 open System.Runtime.CompilerServices
-[<assembly: InternalsVisibleTo(\"Tests\")>]
+[<assembly: InternalsVisibleTo(\"AltCover.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100916443a2ee1d294e8cfa7666fb3f512d998d7ceac4909e35edb2ac1e104de68890a93716d1d1931f7228aac0523cacf50fd82cdb4ccf4ff4bf0ded95e3a383f4f371e3b82c45502ce74d7d572583495208c1905e0f1e8a3cce66c4c75e4ca32e9a8f8dee64e059c0dc0266e8d2cb6d7ebd464b47e062f80b63d390e389217fb7\")>]
 ()
 "
     let file = String.Format(System.Globalization.CultureInfo.InvariantCulture, template) //, token)
@@ -48,13 +50,41 @@ Target "BuildDebug" (fun _ ->
      |> Log "AppBuild-Output: "
 )
 
-Target "Test" (fun _ ->
-    !! (@"_Binaries\Tests\Debug+AnyCPU/Tests.dll")
-      |> NUnit (fun p ->
-          {p with
-             DisableShadowCopy = true;
-             OutputFile = "_/Output/TestResults.xml" })
+Target "TestCover" (fun _ ->
+    ensureDirectory "./_Reports"
+    OpenCover (fun p -> { p with ExePath = findToolInSubPath "OpenCover.Console.exe" "."
+                                 WorkingDir = "."
+                                 TestRunnerExePath = findToolInSubPath "nunit3-console.exe" "."
+                                 Filter = "+[AltCove*]*"
+                                 MergeByHash = true
+                                 Register = RegisterType.RegisterUser
+                                 Output = "_Reports/OpenCoverReport.xml" }) 
+        "_Binaries/AltCover.Tests/Debug+AnyCPU/AltCover.Tests.dll --result=./_Reports/NUnit3Report.xml"
+    ReportGenerator (fun p -> { p with ExePath = findToolInSubPath "Reportgenerator.exe" "."
+                                       TargetDir = "_Reports"})
+        ["./_Reports/OpenCoverReport.xml"]
 )             
+
+Target "Test" (fun _ ->
+    ensureDirectory "./_Reports"
+    !! (@"_Binaries\*Tests\Debug+AnyCPU\*.Tests.dll")
+    |> NUnit3 (fun p -> { p with ToolPath = findToolInSubPath "nunit3-console.exe" "."
+                                 WorkingDir = "."
+                                 ResultSpecs = ["./_Reports/NUnit3Report.xml"] })
+)             
+
+// This defaults to Microsoft Visual Studio 10.0\Team Tools\Static Analysis Tools\FxCop\FxCopCmd.exe
+Target "FxCop" (fun _ ->
+    let fxCop = combinePaths (environVar "VS150COMNTOOLS") "../../Team Tools/Static Analysis Tools/FxCop/FxCopCmd.exe"
+    !! (@"_Binaries\*Tests\Debug+AnyCPU/Altcove*.*") 
+    |> Seq.filter (fun n -> not (n.EndsWith(".Tests.dll")))
+    |> Seq.filter (fun n -> not (n.EndsWith(".pdb")))
+    |> FxCop (fun p -> { p with ToolPath = fxCop
+                                WorkingDir = "."
+                                Verbose = false
+                                ReportFileName = "_Reports/FxCopReport.xml"})
+)
+
 
 "Clean"
 ==> "SetVersion"
@@ -66,4 +96,11 @@ Target "Test" (fun _ ->
 "BuildDebug"
 ==> "Test"
 
-RunTargetOrDefault "Test"
+"BuildDebug"
+==> "TestCover"
+
+"BuildDebug"
+==> "FxCop"
+
+
+RunTargetOrDefault "TestCover"
