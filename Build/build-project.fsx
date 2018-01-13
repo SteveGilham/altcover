40024ad58fadb0b84a46d4cd8f2636c72737ea4c
Steve Gilham <SteveGilham@users.noreply.github.com>
2018-01-13 17:16:25 +0000

    Let's give this a try

---
+++ Build/build-project.fsx
@@ -0,0 +1,13 @@
+#r "C:/Users/steve/Documents/GitHub/altcover/packages/FAKE.4.64.3/tools/FakeLib.dll" // include Fake lib
+#I "C:/Users/steve/Documents/GitHub/altcover/packages/FSharpLint.Fake.0.8.1/tools"
+#r "FSharpLint.Fake.dll"
+#I "C:/Users/steve/Documents/GitHub/altcover/packages/FSharp.Formatting.2.14.4/lib/net40"
+#r "FSharp.Markdown.dll"
+#I "C:/Users/steve/Documents/GitHub/altcover/packages/YamlDotNet.4.2.3/lib/net35"
+#r "YamlDotNet.dll"
+#r "System.IO.Compression.FileSystem.dll"
+#r "System.Xml"
+#r "System.Xml.Linq"
+
+#load "actions.fsx"
+#load "targets.fsx"
