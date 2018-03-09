fa3557ec8d0893ca370d5eaffd419c117383526f
Steve Gilham <SteveGilham@users.noreply.github.com>
2018-03-09 20:17:46 +0000

    Try building via solution to get AnyCPU flavour assemblies, then drive with the x86 runtime

--- Build/targets.fsx
+++ Build/targets.fsx
@@ -46,13 +46,13 @@ let monoOnWindows = if isWindows then
                        |> List.tryFind (fun _ -> true)
                     else None
 
-let dotnetX86 = if isWindows then
+let dotnetPath86 = if isWindows then
                        [programFiles86]
                        |> List.filter (String.IsNullOrWhiteSpace >> not)
                        |> List.map (fun s -> s @@ "dotnet\dotnet.EXE")
                        |> List.filter File.Exists
                        |> List.tryFind (fun _ -> true)
-                else None
+                   else None
 
 let nugetCache = Path.Combine (Environment.GetFolderPath Environment.SpecialFolder.UserProfile,
                                ".nuget/packages")
@@ -1158,7 +1158,7 @@ Target "ReleaseFSharpTypesX86DotNetRunner" ( fun _ ->
     let unpack = Path.getFullName "_Packaging/Unpack/tools/netcoreapp2.0/AltCover"
     let x = Path.getFullName "./_Reports/AltCoverReleaseFSharpTypesX86DotNetRunner.xml"
     let o = Path.getFullName "Sample2/_Binaries/Sample2/Debug+x86/netcoreapp2.0"
-    let i = Path.getFullName "_Binaries/Sample2/Debug+x86/netcoreapp2.0"
+    let i = Path.getFullName "_Binaries/Sample2/Debug+AnyCPU/netcoreapp2.0"
 
     Shell.CleanDir o
     try
@@ -1167,7 +1167,7 @@ Target "ReleaseFSharpTypesX86DotNetRunner" ( fun _ ->
 
         Actions.Run (fun info ->
             { info with
-                FileName = dotnetX86 |> Option.get
+                FileName = dotnetPath86 |> Option.get
                 Arguments = "--info"}) "dotnet-x86 failed"
 
         printfn "Build the code"
@@ -1178,13 +1178,13 @@ Target "ReleaseFSharpTypesX86DotNetRunner" ( fun _ ->
                     Configuration = BuildConfiguration.Debug
                     Common = dotnetOptions p.Common})
         Actions.RunDotnet (fun o' -> {dotnetOptions o' with WorkingDirectory = unpack }) "build"
-                      (" altcover.core.fsproj --configuration Release")
+                      (" ../altcover.dotnet.sln --configuration Release")
                       "ReleaseFSharpTypesX86DotNetRunnerBuild"
 
         printfn "Instrument the code"
-        let altcover = unpack @@ "_Binaries/AltCover/Release+x86/netcoreapp2.0/AltCover.dll"
+        let altcover = unpack @@ "../_Binaries/AltCover/Release+AnyCPU/netcoreapp2.0/AltCover.dll"
         Actions.RunDotnet (fun o' -> {dotnetOptions o' with WorkingDirectory = unpack
-                                                            DotNetCliPath = dotnetX86 |> Option.get}) ""
+                                                            DotNetCliPath = dotnetPath86 |> Option.get}) ""
                       (altcover + " -x \"" + x + "\" -o \"" + o + "\" -i \"" + i + "\"")
                       "ReleaseFSharpTypesX86DotNetRunner"
 
@@ -1195,9 +1195,10 @@ Target "ReleaseFSharpTypesX86DotNetRunner" ( fun _ ->
 
         // Run
         Actions.RunDotnet (fun o' -> {dotnetOptions o' with WorkingDirectory = o
-                                                            DotNetCliPath = dotnetX86 |> Option.get}) ""
-                              (altcover + " Runner -x \"" + (dotnetX86 |> Option.get) +
-                              "\" -r \"" + o + "\" -- test --no-build --configuration Debug " + sample2)
+                                                            DotNetCliPath = dotnetPath86 |> Option.get}) ""
+                              (altcover + " Runner -x \"" + (dotnetPath |> Option.get) +
+                              "\" -r \"" + o + "\" -- test --no-build --configuration Debug " + sample2 + 
+                              " -- TargetPlatform=x86")
                             "ReleaseFSharpTypesX86DotNetRunner test"
 
         Actions.ValidateFSharpTypesCoverage x
@@ -1449,7 +1450,7 @@ ActivateFinal "ResetConsoleColours"
 
 "Unpack"
 ==> "ReleaseFSharpTypesX86DotNetRunner"
-=?> ("Deployment", Option.isSome dotnetX86)
+=?> ("Deployment", Option.isSome dotnetPath86)
 
 "Unpack"
 ==> "ReleaseXUnitFSharpTypesDotNet"
