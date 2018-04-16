d0ce6de984578a88eb9af6ce0c70af102c5427cb
Steve Gilham <SteveGilham@users.noreply.github.com>
2018-04-16 11:39:01 +0100

    Expand the stub (Gendarme doesn't like this step)

--- AltCover/Cobertura.fs
+++ AltCover/Cobertura.fs
@@ -6,7 +6,15 @@ open System.Xml.Linq
 module Cobertura =
   let internal path : Option<string> ref = ref None
 
-  let Summary //(report:XDocument) (format:Base.ReportFormat) result =
-              _ _ _ =
+  let NCover _  = //(report:XDocument)
     NotImplementedException() |> raise
+
+  let OpenCover _  = //(report:XDocument)
+    NotImplementedException() |> raise
+
+  let Summary (report:XDocument) (format:Base.ReportFormat) result =
+    match format with 
+    | Base.ReportFormat.NCover -> NCover report
+    | _ -> OpenCover report
+    result
 
