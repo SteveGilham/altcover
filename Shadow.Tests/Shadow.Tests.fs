0b4e56caf3d7f33767b34b6a133161e3a96399de
Steve Gilham <SteveGilham@users.noreply.github.com>
2018-05-05 20:02:54 +0100

    Synchronous in batches seems to be the best we can do.

--- Shadow.Tests/Shadow.Tests.fs
+++ Shadow.Tests/Shadow.Tests.fs
@@ -178,21 +178,18 @@ type AltCoverTests() = class
     lock Instance.Visits (fun () ->
     Instance.Capacity <- 0
     let save = Instance.trace
-    let wait = Instance.Wait
     try
       Instance.Visits.Clear()
       Instance.RunMailbox()
       Instance.trace <- { Tracer=null; Stream=null; Formatter=null;
                           Runner = false; Definitive = false }
       let key = " "
-      Instance.Wait <- System.Threading.Timeout.Infinite // force synchronous
       Instance.VisitSelection (fun () -> true) Null key 23
       Assert.That (Instance.Visits.Count, Is.EqualTo 1, "A visit that should have happened, didn't")
       Assert.That (Instance.Visits.[key].Count, Is.EqualTo 1, "keys = " + String.Join("; ", Instance.Visits.Keys|> Seq.toArray))
       Assert.That (Instance.Visits.[key].[23], Is.EqualTo (1, []))
     finally
       Instance.Visits.Clear()
-      Instance.Wait <- wait
       Instance.trace <- save)
     self.GetMyMethodName "<="
 
