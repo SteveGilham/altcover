f6722543b57481f7ac99bf9b6ef37544df5c045c
Steve Gilham <SteveGilham@users.noreply.github.com>
2018-05-24 21:11:46 +0100

    Revert "Experimental -- dotnet tooling"
    This reverts commit 6e41c67b379e1baa81cec377b9fe79b346146058.

--- Build/actions.fsx
+++ Build/actions.fsx
@@ -63,7 +63,6 @@ open System.Runtime.CompilerServices
 #else
 #if NETCOREAPP2_0
 [<assembly: InternalsVisibleTo("AltCover.PowerShell")>]
-[<assembly: InternalsVisibleTo("dotnet-altcover")>]
 [<assembly: InternalsVisibleTo("AltCover.Tests")>]
 [<assembly: InternalsVisibleTo("AltCover.XTests")>]
 
