67738753fc268b88e25b09a4cf34ff4d56cfbf2a
Steve Gilham <SteveGilham@users.noreply.github.com>
2018-06-07 13:57:16 +0100

    Complete the Invoke-Altcover tests and fix the build

--- Build/Pester.Tests.ps1
+++ Build/Pester.Tests.ps1
@@ -4,10 +4,19 @@ $i = "./_Binaries/Sample2/Debug+AnyCPU/netcoreapp2.0"
 
 Describe "Invoke-Altcover" {
     It "instruments and collects" {
-        Invoke-AltCover -XmlReport $x -OutputDirectory  $o -InputDirectory $i -InPlace
+        if (Test-Path $o) {
+            Remove-Item -Force -Recurse $o
+        }
+        Invoke-AltCover -XmlReport $x -OutputDirectory  $o -InputDirectory $i -AssemblyFilter "Adapter" -InformationAction Continue
         $o | Should -Exist
         $x | Should -Exist
-        Invoke-AltCover -Runner -RecorderDirectory $i -WorkingDirectory "./Sample2" -Executable "dotnet" -CommandLine @("test", "--no-build", "--configuration", "Debug",  "sample2.core.fsproj")
+        $xm = [xml](Get-Content $x)
+        [string]::Join(" ", $xm.coverage.module.method.name) | Should -Be "main returnFoo returnBar testMakeUnion as_bar get_MyBar Invoke .ctor makeThing testMakeThing bytes"
+        [string]::Join(" ", $xm.coverage.module.method.seqpnt.visitcount) | Should -Be "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0"
+        Invoke-AltCover  -InformationAction Continue -Runner -RecorderDirectory $o -WorkingDirectory "./Sample2" -Executable "dotnet" -CommandLine @("test", "--no-build", "--configuration", "Debug",  "sample2.core.fsproj")
+        $xm2 = [xml](Get-Content $x)
+        [string]::Join(" ", $xm2.coverage.module.method.seqpnt.visitcount) | Should -Be "0 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 2 1 1 1"
+        Remove-Item -Force -Recurse $o
     }
 
     It "Fails on garbage" {
