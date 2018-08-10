d3160a39617f167e9fd5cd4dcaa0aac81e0d1195
Steve Gilham <SteveGilham@users.noreply.github.com>
2018-08-10 19:23:55 +0100

    Simple Cake API

--- AltCover.Cake/Cake.cs
+++ AltCover.Cake/Cake.cs
@@ -1,12 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Cake.Core;
+using Cake.Core.Annotations;
+using LogLevel = Cake.Core.Diagnostics.LogLevel;
+using Verbosity = Cake.Core.Diagnostics.Verbosity;
 
 namespace AltCover.Cake
 {
-    public class Cake
+    public static class Api
     {
+        private static LogArgs MakeLog(ICakeContext context, LogArgs l)
+        {
+            if (l != null)
+                return l;
+
+            if (context != null)
+                return new LogArgs()
+                {
+                    Info = x => context.Log.Write(Verbosity.Normal, LogLevel.Information, x),
+                    Warn = x => context.Log.Write(Verbosity.Normal, LogLevel.Warning, x),
+                    Echo = x => context.Log.Write(Verbosity.Normal, LogLevel.Error, x),
+                    Error = x => context.Log.Write(Verbosity.Verbose, LogLevel.Information, x),
+                };
+
+            return new LogArgs()
+            {
+                Info = x => { },
+                Warn = x => { },
+                Echo = x => { },
+                Error = x => { }
+            };
+        }
+
+        [CakeMethodAlias]
+        public static int Prepare(this ICakeContext context, PrepareArgs p, LogArgs l = null)
+        {
+            return CSApi.Prepare(p, MakeLog(context, l));
+        }
+
+        [CakeMethodAlias]
+        public static int Collect(this ICakeContext context, CollectArgs c, LogArgs l = null)
+        {
+            return CSApi.Collect(c, MakeLog(context, l));
+        }
+
+        [CakeMethodAlias]
+        public static int Ipmo(this ICakeContext context, LogArgs l = null)
+        {
+            return CSApi.Ipmo(MakeLog(context, l));
+        }
+
+        [CakeMethodAlias]
+        public static int Version(this ICakeContext context, LogArgs l = null)
+        {
+            return CSApi.Version(MakeLog(context, l));
+        }
     }
 }
