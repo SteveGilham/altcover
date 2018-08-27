51df1dbd129882dbf665bc025e9b22a8368d6933
Steve Gilham <SteveGilham@users.noreply.github.com>
2018-08-27 18:06:29 +0100

    Shared code placeholder

---
+++ AltCover.PowerShell/RenderToHtml.fs
@@ -0,0 +1,12 @@
+﻿namespace AltCover
+
+open System.Diagnostics.CodeAnalysis
+open System.Xml
+
+module RenderToHtml =
+  [<SuppressMessage("Microsoft.Design", "CA1059",
+    Justification="Premature abstraction")>]
+  let Action x = 
+    let doc = XmlDocument()
+    doc.CreateComment(x.ToString()) |> doc.AppendChild |> ignore
+    doc
