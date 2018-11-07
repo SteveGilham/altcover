f6b21f7bc0b6eac93b53c4de1f9b8d968f35735c
Steve Gilham <SteveGilham@users.noreply.github.com>
2018-11-07 15:08:57 +0000

    Fix indent levels from fantomas

--- AltCover.Fake/Fake.fs
+++ AltCover.Fake/Fake.fs
@@ -1,5 +1,5 @@
 #if FAKEAPI
-module AltCover_Fake.DotNet.DotNet
+namespace AltCover_Fake.DotNet
 
 open System
 open System.Reflection
@@ -34,81 +34,82 @@ type Api =
   static member Ipmo() = AltCover.Api.Ipmo()
   static member Version() = AltCover.Api.Version()
 
-module DotNet =
 #endif
 
-open Fake.DotNet
+module DotNet =
+  open Fake.DotNet
 
-type DotNet.TestOptions with
+  type DotNet.TestOptions with
 #if FAKEAPI
-  member self.WithParameters (prepare : AltCover.PrepareParams)
-         (collect : AltCover.CollectParams) =
+    member self.WithParameters (prepare : AltCover.PrepareParams)
+           (collect : AltCover.CollectParams) =
 #else
     member self.WithParameters (prepare : PrepareParams) (collect : CollectParams) =
 #endif
-    let options = DotNet.ToTestArguments prepare collect
-    let custom = self.Common.CustomParams
 
-    let extended =
-      match custom with
-      | None -> Some options
-      | Some thing -> Some(thing + " " + options)
+      let options = DotNet.ToTestArguments prepare collect
+      let custom = self.Common.CustomParams
 
-    // was
-    //      { self with Common = { self.Common with CustomParams = extended } }
-    // which really means
-    // 	return new global::Fake.DotNet.DotNet.TestOptions(
-    //new global::Fake.DotNet.DotNet.Options(
-    //        common.DotNetCliPath,
-    //        common.WorkingDirectory,
-    //        extended,
-    //        common.Verbosity,
-    //        common.Diagnostics,
-    //        common.RedirectOutput,
-    //        common.Environment),
-    //    self.Settings,
-    //    self.ListTests,
-    //    self.Filter,
-    //    self.TestAdapterPath,
-    //    self.Logger,
-    //    self.Configuration,
-    //    self.Framework,
-    //    self.Output,
-    //    self.Diag,
-    //    self.NoBuild,
-    //    self.ResultsDirectory,
-    //    self.Collect,
-    //    self.NoRestore,
-    //    self.RunSettingsArguments);
-    // where the constructors are version dependent
-    let OptionsConstructor = self.Common.GetType().GetConstructors().[0]
+      let extended =
+        match custom with
+        | None -> Some options
+        | Some thing -> Some(thing + " " + options)
 
-    let args =
-      OptionsConstructor.GetParameters()
-      |> Array.map (fun info ->
-           let t = info.ParameterType
-           if t.GetTypeInfo().IsValueType then Activator.CreateInstance(t)
-           else null)
+      // was
+      //      { self with Common = { self.Common with CustomParams = extended } }
+      // which really means
+      // 	return new global::Fake.DotNet.DotNet.TestOptions(
+      //new global::Fake.DotNet.DotNet.Options(
+      //        common.DotNetCliPath,
+      //        common.WorkingDirectory,
+      //        extended,
+      //        common.Verbosity,
+      //        common.Diagnostics,
+      //        common.RedirectOutput,
+      //        common.Environment),
+      //    self.Settings,
+      //    self.ListTests,
+      //    self.Filter,
+      //    self.TestAdapterPath,
+      //    self.Logger,
+      //    self.Configuration,
+      //    self.Framework,
+      //    self.Output,
+      //    self.Diag,
+      //    self.NoBuild,
+      //    self.ResultsDirectory,
+      //    self.Collect,
+      //    self.NoRestore,
+      //    self.RunSettingsArguments);
+      // where the constructors are version dependent
+      let OptionsConstructor = self.Common.GetType().GetConstructors().[0]
 
-    let common = OptionsConstructor.Invoke(args)
-    self.Common.GetType().GetFields(BindingFlags.NonPublic ||| BindingFlags.Instance)
-    |> Array.iter (fun f ->
-         f.SetValue(common, (
-                    if f.Name <> "CustomParams@" then f.GetValue self.Common
-                    else extended :> obj)))
-    let TestOptionsConstructor = self.GetType().GetConstructors().[0]
+      let args =
+        OptionsConstructor.GetParameters()
+        |> Array.map (fun info ->
+             let t = info.ParameterType
+             if t.GetTypeInfo().IsValueType then Activator.CreateInstance(t)
+             else null)
 
-    let args' =
-      TestOptionsConstructor.GetParameters()
-      |> Array.map (fun info ->
-           let t = info.ParameterType
-           if t.GetTypeInfo().IsValueType then Activator.CreateInstance(t)
-           else null)
+      let common = OptionsConstructor.Invoke(args)
+      self.Common.GetType().GetFields(BindingFlags.NonPublic ||| BindingFlags.Instance)
+      |> Array.iter (fun f ->
+           f.SetValue(common,
+                      (if f.Name <> "CustomParams@" then f.GetValue self.Common
+                       else extended :> obj)))
+      let TestOptionsConstructor = self.GetType().GetConstructors().[0]
 
-    let result = TestOptionsConstructor.Invoke(args')
-    self.GetType().GetFields(BindingFlags.NonPublic ||| BindingFlags.Instance)
-    |> Array.iter (fun f ->
-         f.SetValue(result, (
-                    if f.Name <> "Common@" then f.GetValue self
-                    else common)))
-    result :?> DotNet.TestOptions
+      let args' =
+        TestOptionsConstructor.GetParameters()
+        |> Array.map (fun info ->
+             let t = info.ParameterType
+             if t.GetTypeInfo().IsValueType then Activator.CreateInstance(t)
+             else null)
+
+      let result = TestOptionsConstructor.Invoke(args')
+      self.GetType().GetFields(BindingFlags.NonPublic ||| BindingFlags.Instance)
+      |> Array.iter (fun f ->
+           f.SetValue(result,
+                      (if f.Name <> "Common@" then f.GetValue self
+                       else common)))
+      result :?> DotNet.TestOptions
