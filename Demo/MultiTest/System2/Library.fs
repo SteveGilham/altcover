867b64d0194bf37536a80bfe6a44026cc1dddded
Steve Gilham <SteveGilham@users.noreply.github.com>
2018-12-12 08:49:21 +0000

    Example of the .net SDK 2.1.500 solution-level `dotnet test`

---
+++ Demo/MultiTest/System2/Library.fs
@@ -0,0 +1,33 @@
+namespace System2
+
+open System
+open System.Reflection
+
+[<assembly:AssemblyVersionAttribute("1.0.0.0")>]
+[<assembly:AssemblyFileVersionAttribute("1.0.0.0")>]
+do ()
+
+module DU =
+  type MyUnion =
+    | Foo of int
+    | Bar of string
+    | Baz of double
+    | Bop of DateTime
+
+    member this.as_bar() =
+      try
+        match this with
+        | Foo n -> Bar(string n)
+        | Baz d -> Bar(string d)
+        | Bop t -> Bar(string t)
+        // New cases go in here
+        | _ -> this
+      with _ -> Bar "none"
+
+    member this.MyBar = this.as_bar
+
+  type MyClass() =
+    member val Property = 0 with get, set
+
+  let returnFoo v = Foo v
+  let returnBar v = Bar v
