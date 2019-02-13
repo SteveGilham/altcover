da56893735a5309904507ec07fff85ad687e45e2
Steve Gilham <SteveGilham@users.noreply.github.com>
2019-02-13 21:18:04 +0000

    Add a test program with sourcelink

---
+++ Sample14/Sample14/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Sample14
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var x = new Sample5.Class1();
+            Console.WriteLine(x.F1("Hello World!"));
+        }
+    }
+}
