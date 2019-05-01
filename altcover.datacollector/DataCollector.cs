45afeae0ccd4e4d99c64c46ae2506df4b4cd217d
Steve Gilham <SteveGilham@users.noreply.github.com>
2019-05-01 21:02:46 +0100

    Getting away from F# issues with dependencies and interface implementation, and using `dotnet test --settings recorder.runsettings` actually invokes the code.

---
+++ altcover.datacollector/DataCollector.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.DataCollection;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.DataCollector.InProcDataCollector;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.InProcDataCollector;
+
+namespace AltCover.DataCollector
+{
+    public class Recorder : InProcDataCollection
+    {
+        public void Initialize(IDataCollectionSink _dataCollectionSink)
+        {
+            System.IO.File.WriteAllText(@"C:\Users\steve\Documents\GitHub\altcover\Initialize.txt",
+                                        "Initialize!");
+            //sprintf "Test Session Initialize" |> Debug.WriteLine
+            //    AppDomain.CurrentDomain.GetAssemblies()
+            //|> Seq.iter(sprintf "    %A" >> Debug.WriteLine)
+        }
+
+        public void TestCaseEnd(TestCaseEndArgs _testCaseEndArgs)
+        { }
+
+        public void TestCaseStart(TestCaseStartArgs _testCaseStartArgs)
+        { }
+
+        public void TestSessionEnd(TestSessionEndArgs _testSessionEndArgs)
+        {
+            //    sprintf "Test Session Initialize" |> Debug.WriteLine
+            //    AppDomain.CurrentDomain.GetAssemblies()
+            //|> Seq.iter(sprintf "    %A" >> Debug.WriteLine)
+        }
+
+        public void TestSessionStart(TestSessionStartArgs _testSessionStartArgs)
+        {
+            //System.IO.File.WriteAllText(@"C:\Users\steve\Documents\GitHub\altcover\datacollector.txt", "TestSessionStart!")
+            //sprintf "Test Session Initialize" |> Debug.WriteLine
+            //AppDomain.CurrentDomain.GetAssemblies()
+            //|> Seq.iter(sprintf "    %A" >> Debug.WriteLine)
+        }
+    }
+}
