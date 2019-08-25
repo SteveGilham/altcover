b91ffc3951e3e097f0c8a512d3d8c8e4ea013d6d
Steve Gilham <SteveGilham@users.noreply.github.com>
2019-08-25 15:27:45 +0100

    Display the source in the files, w/o any annotations as yet

--- AltCover.FSApi/RenderToHtml.fs
+++ AltCover.FSApi/RenderToHtml.fs
@@ -4,6 +4,7 @@ open System.Diagnostics.CodeAnalysis
 open System.IO
 open System.Xml
 open System.Xml.XPath
+open System
 
 module RenderToHtml =
   [<SuppressMessage("Microsoft.Design", "CA1059",
@@ -20,6 +21,7 @@ module RenderToHtml =
                 |> Seq.distinct
 
     files // TODO handle repeated names in different folders
+    |> Seq.filter File.Exists
     |> Seq.map (fun path ->
             let doc = XmlDocument()
             [
@@ -28,5 +30,61 @@ module RenderToHtml =
              doc.CreateElement("html")  :> XmlNode
             ]
             |> List.iter (doc.AppendChild >> ignore)
+
+            let head = doc.CreateElement("head")
+            head
+            |> doc.DocumentElement.AppendChild
+            |> ignore
+
+            let meta = doc.CreateElement("meta")
+            head.AppendChild meta |> ignore
+            meta.SetAttribute("http-equiv", "content-type")
+            meta.SetAttribute("content", "text/html; charset=utf-8")
+            let title = doc.CreateElement("title")
+            head.AppendChild title |> ignore
+            title.InnerText <- path
+            let style = doc.CreateElement("style")
+            head.AppendChild style |> ignore
+            style.InnerText <- [
+              "body { background: white; }"
+              "td.code"
+              "{"
+              "  font-family: \"Fira Code\", Consolas, \"Lucida Console\", \"Courier New\", monospace;"
+              "  color: #c0c0c0;"
+              "}"
+              "td.plain"
+              "{"
+              "  color: white; font-weight: bold;"
+              "}"
+            ] |> (fun l -> String.Join(Environment.NewLine, l))
+
+      // "visited", "#404040", "#cefdce") // "#98FB98") ; // Dark on Pale Green
+      //("declared", "#FFA500", "#FFFFFF") // Orange on White
+      //("static", "#808080", "#F5F5F5") // Grey on White Smoke
+      //("automatic", "#808080", "#FFFF00") // Grey on Yellow
+      //("notVisited", "#ff0000", "#FFFFFF") // Red on White
+      //("excluded", "#87CEEB", "#FFFFFF") // Sky Blue on white
+
+            let body = doc.CreateElement("body")
+            body
+            |> doc.DocumentElement.AppendChild
+            |> ignore
+
+            let table = doc.CreateElement("table")
+            body.AppendChild table |> ignore
+
+            File.ReadAllLines path
+            |> Seq.iter (fun l ->
+              let row = doc.CreateElement("tr")
+              table.AppendChild row |> ignore
+              let cell = doc.CreateElement("td")
+              cell.InnerText <- "\u00A0\u2442\u00A0"
+              cell.SetAttribute("class", "plain")
+              row.AppendChild cell |> ignore
+              let cell2 = doc.CreateElement("td")
+              row.AppendChild cell2 |> ignore
+              cell2.SetAttribute("class", "code")
+              cell2.InnerText <- l
+            )
 
             (Path.GetFileName path, doc))
