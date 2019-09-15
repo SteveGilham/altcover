5bbac1275c43044fcee13f9e774b382cacb0a3e2
Steve Gilham <SteveGilham@users.noreply.github.com>
2019-09-15 11:36:47 +0100

    Add some real-world examples

---
+++ Sample16/Foo.cs
@@ -0,0 +1,31 @@
+﻿namespace Sample16
+{
+    public static class Foo
+    {
+        public static string Bar(string value)
+        {
+            string ret;
+
+            switch (value)
+            {
+                case "a":
+                    ret = "a";
+                    break;
+
+                case "b":
+                    ret = "b";
+                    break;
+
+                case "c":
+                    ret = "c";
+                    break;
+
+                default:
+                    ret = "d";
+                    break;
+            }
+
+            return ret;
+        }
+    }
+}
