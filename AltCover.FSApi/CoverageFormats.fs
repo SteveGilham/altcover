2f1837497e47eb9c9621be90c8947feaa18eec35
Steve Gilham <SteveGilham@users.noreply.github.com>
2020-02-13 14:21:41 +0000

    First couple of items

--- AltCover.FSApi/CoverageFormats.fs
+++ AltCover.FSApi/CoverageFormats.fs
@@ -149,6 +149,44 @@ module CoverageFormats =
       System.Threading.Thread.CurrentThread.CurrentCulture <- culture
     rewrite
 
+  let internal X s = XName.Get s
+
+  let internal Summary() =
+    XElement
+      (X "Summary", XAttribute(X "numSequencePoints", 0),
+        XAttribute(X "visitedSequencePoints", 0), XAttribute(X "numBranchPoints", 0),
+        XAttribute(X "visitedBranchPoints", 0), XAttribute(X "sequenceCoverage", 0),
+        XAttribute(X "branchCoverage", 0), XAttribute(X "maxCyclomaticComplexity", 0),
+        XAttribute(X "minCyclomaticComplexity", 0), XAttribute(X "visitedClasses", 0),
+        XAttribute(X "numClasses", 0), XAttribute(X "visitedMethods", 0),
+        XAttribute(X "numMethods", 0), XAttribute(X "minCrapScore", 0),
+        XAttribute(X "maxCrapScore", 0))
+
   let FormatFromCoverlet (report:XDocument) (files:string array) =
     let rewrite = XDocument(report)
+    // attributes in <CoverageSession xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+
+    rewrite.Descendants(X "CoverageSession")
+    |> Seq.iter(fun session -> session.RemoveAttributes()
+                               session.Add(XAttribute(XNamespace.Xmlns + "xsd",
+                                            "http://www.w3.org/2001/XMLSchema"),
+                                           XAttribute(XNamespace.Xmlns + "xsi",
+                                            "http://www.w3.org/2001/XMLSchema-instance")))
+
+    // supply empty module level  <Summary numSequencePoints="0" visitedSequencePoints="0" numBranchPoints="0" visitedBranchPoints="0" sequenceCoverage="0" branchCoverage="0" maxCyclomaticComplexity="0" minCyclomaticComplexity="0" visitedClasses="0" numClasses="0" visitedMethods="0" numMethods="0" minCrapScore="0" maxCrapScore="0" />
+    rewrite.Descendants(X "Module")
+    |> Seq.iter (fun m -> (m.Elements() |> Seq.head).AddBeforeSelf(Summary()))
+
+    // TODO list
+    // fix hash in <Module hash="42-08-CA-1A-A6-25-CE-DA-DD-18-DC-D5-9A-BF-1B-BF-00-1D-E5-9B">
+    // complete module level  <Summary numSequencePoints="23" visitedSequencePoints="10" numBranchPoints="21" visitedBranchPoints="7" sequenceCoverage="43.48" branchCoverage="33.33" maxCyclomaticComplexity="11" minCyclomaticComplexity="1" visitedClasses="4" numClasses="7" visitedMethods="7" numMethods="11" minCrapScore="1" maxCrapScore="62.05" />
+    // fill in path  <ModulePath>...\Sample4.dll</ModulePath>
+    // visited attribute <Method visited="false" cyclomaticComplexity="1" nPathComplexity="0" sequenceCoverage="0" branchCoverage="0" isConstructor="false" isStatic="true" isGetter="false" isSetter="false" crapScore="0">
+    // value in method <MetadataToken>100663297</MetadataToken>
+    // xsi:type in <MethodPoint xsi:type="SequencePoint" vc="0" uspid="0" ordinal="0" offset="2" sl="59" sc="16" el="59" ec="17" bec="0" bev="0" fileid="1" />
+    //  instead of xmlns:p8="xsi" <MethodPoint vc="0" uspid="0" p8:type="SequencePoint" ordinal="0" offset="0" sc="0" sl="59" ec="1" el="59" bec="0" bev="0" fileid="1" xmlns:p8="xsi" />
+    // Fix sc, ec in <MethodPoint />
+    // offset, sc, ec in  <SequencePoint vc="1" uspid="1" ordinal="0" offset="0" sl="47" sc="21" el="47" ec="26" bec="0" bev="0" fileid="1" />
+    // compute bec and bev values
+
     rewrite
