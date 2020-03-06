2b35f67b852d5f831137f916de2212ca78b073ce
Steve Gilham <SteveGilham@users.noreply.github.com>
2020-03-06 09:06:13 +0000

    Gendarme clean

--- altcover.datacollector/DataCollector.cs
+++ altcover.datacollector/DataCollector.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
 
@@ -9,94 +10,103 @@ using Microsoft.VisualStudio.TestPlatform.ObjectModel.InProcDataCollector;
 
 namespace AltCover
 {
-    public class DataCollector : InProcDataCollection
+  public class DataCollector : InProcDataCollection
+  {
+    [SuppressMessage("Gendarme.Rules.Correctness", "MethodCanBeMadeStaticRule"),
+     SuppressMessage("Gendarme.Rules.Smells", "RelaxedAvoidCodeDuplicatedInSameClassRule")]
+    private void Supervise()
     {
-        private void Supervise()
+      var rec =
+      AppDomain.CurrentDomain.GetAssemblies()
+          .Where(a => a.GetName().Name == "AltCover.Recorder.g")
+          .FirstOrDefault();
+      if (rec == null)
+      {
+        Debug.WriteLine("Recorder not found");
+      }
+      else
+      {
+        var i = rec.GetTypes()
+            .Where(t => t.Name == "Instance")
+            .FirstOrDefault();
+        if (i == null)
         {
-            var rec =
-            AppDomain.CurrentDomain.GetAssemblies()
-                .Where(a => a.GetName().Name == "AltCover.Recorder.g")
-                .FirstOrDefault();
-            if (rec == null)
-            {
-                Debug.WriteLine("Recorder not found");
-            }
-            else
-            {
-                var i = rec.GetTypes()
-                    .Where(t => t.Name == "Instance")
-                    .FirstOrDefault();
-                if (i == null)
-                {
-                    Debug.WriteLine("Instance not found");
-                }
-                else
-                {
-                    var supervision = i.GetProperty("Supervision", BindingFlags.Static | BindingFlags.Public);
-                    if (supervision == null)
-                    {
-                        Debug.WriteLine("Supervision not found");
-                    }
-                    else
-                    {
-                        supervision.SetValue(null, true);
-                    }
-                }
-            }
+          Debug.WriteLine("Instance not found");
         }
-
-        public void Initialize(IDataCollectionSink _dataCollectionSink)
+        else
         {
-            Debug.WriteLine("Initialize");
-            Supervise();
+          var supervision = i.GetProperty("Supervision", BindingFlags.Static | BindingFlags.Public);
+          if (supervision == null)
+          {
+            Debug.WriteLine("Supervision not found");
+          }
+          else
+          {
+            supervision.SetValue(null, true);
+          }
         }
-
-        public void TestCaseEnd(TestCaseEndArgs _testCaseEndArgs)
-        { }
-
-        public void TestCaseStart(TestCaseStartArgs _testCaseStartArgs)
-        {
-        }
-
-        public void TestSessionEnd(TestSessionEndArgs _testSessionEndArgs)
-        {
-            Debug.WriteLine("TestSessionEnd");
-            var rec =
-            AppDomain.CurrentDomain.GetAssemblies()
-                .Where(a => a.GetName().Name == "AltCover.Recorder.g")
-                .FirstOrDefault();
-            if (rec == null)
-            {
-                Debug.WriteLine("Recorder not found");
-            }
-            else
-            {
-                var i = rec.GetTypes()
-                    .Where(t => t.Name == "Instance")
-                    .FirstOrDefault();
-                if (i == null)
-                {
-                    Debug.WriteLine("Instance not found");
-                }
-                else
-                {
-                    var flush = i.GetMethod("FlushFinish", BindingFlags.Static | BindingFlags.Public);
-                    if (flush == null)
-                    {
-                        Debug.WriteLine("Flush not found");
-                    }
-                    else
-                    {
-                        flush.Invoke(null, null);
-                    }
-                }
-            }
-        }
-
-        public void TestSessionStart(TestSessionStartArgs _testSessionStartArgs)
-        {
-            Debug.WriteLine("TestSessionStart");
-            Supervise();
-        }
+      }
     }
+
+    public void Initialize(IDataCollectionSink dataCollectionSink)
+    {
+      Debug.WriteLine("Initialize {0}", dataCollectionSink);
+      Supervise();
+    }
+
+    public void TestCaseEnd(TestCaseEndArgs testCaseEndArgs)
+    {
+#if DEBUG
+      Debug.WriteLine("TestCaseEnd {0}", testCaseEndArgs);
+#endif
+    }
+
+    public void TestCaseStart(TestCaseStartArgs testCaseStartArgs)
+    {
+#if DEBUG
+      Debug.WriteLine("TestCaseStart {0}", testCaseStartArgs);
+#endif
+    }
+
+    public void TestSessionEnd(TestSessionEndArgs testSessionEndArgs)
+    {
+      Debug.WriteLine("TestSessionEnd {0}", testSessionEndArgs);
+      var rec =
+      AppDomain.CurrentDomain.GetAssemblies()
+          .Where(a => a.GetName().Name == "AltCover.Recorder.g")
+          .FirstOrDefault();
+      if (rec == null)
+      {
+        Debug.WriteLine("Recorder not found");
+      }
+      else
+      {
+        var i = rec.GetTypes()
+            .Where(t => t.Name == "Instance")
+            .FirstOrDefault();
+        if (i == null)
+        {
+          Debug.WriteLine("Instance not found");
+        }
+        else
+        {
+          var flush = i.GetMethod("FlushFinish", BindingFlags.Static | BindingFlags.Public);
+          if (flush == null)
+          {
+            Debug.WriteLine("Flush not found");
+          }
+          else
+          {
+            flush.Invoke(null, null);
+          }
+        }
+      }
+    }
+
+    public void TestSessionStart(TestSessionStartArgs testSessionStartArgs)
+    {
+      Debug.WriteLine("TestSessionStart : {0}", testSessionStartArgs);
+      Supervise();
+    }
+  }
 }
