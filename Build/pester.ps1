5d5e9afc544842f34f43ef41c9aae461c4ec135b
Steve Gilham <SteveGilham@users.noreply.github.com>
2020-04-27 20:40:25 +0100

    Start document generation

--- Build/pester.ps1
+++ Build/pester.ps1
@@ -5,3 +5,10 @@ Import-Module "./packages/pester/4.10.1/tools/Pester.psm1"
 
 Invoke-Altcover -?
 Invoke-Pester -Script @{ Path='.\Build'; Parameters = @{ ACV = $ACV}} -EnableExit -OutputFormat NUnitXml -OutputFile "./_Reports/PesterReport.xml"
+
+$m = Get-Module -Name "AltCover.PowerShell"
+$m.ExportedCmdlets.Keys
+
+$m.ExportedCmdlets.Keys | % { 
+  Invoke-Expression ($_ + " -?")
+}
