ac6a7b1c300250c4fd7a912f4a4b4b9b0c62f84a
Steve Gilham <SteveGilham@users.noreply.github.com>
2020-07-04 12:36:27 +0100

    The original source of the F#/.net 2.0 coverage sample

---
+++ Sample19/ConsoleApplication1/Program.fs
@@ -0,0 +1,12 @@
+﻿let Example (x : int option) =
+    x
+    |> Option.iter (fun i -> Seq.initInfinite id
+                             |> Seq.take i
+                             |> Seq.iter (printfn "%A"))
+
+[<EntryPoint>]
+let main argv = 
+    printfn "CLR Version %A" <| System.Environment.Version.ToString()
+    Example None
+    printfn "Done"
+    0
