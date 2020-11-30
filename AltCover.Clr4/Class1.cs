d6e3322ca50f3e8b55120ab45f8d95573608fcd1
Steve Gilham <SteveGilham@users.noreply.github.com>
2020-11-30 10:35:29 +0000

    Try a light-weight alternative

---
+++ AltCover.Clr4/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace AltCover.Clr4
+{
+  public class Class1<T>
+  {
+    private static AsyncLocal<T> instance;
+
+    public AsyncLocal<T> Instance()
+    {
+      return instance;
+    }
+
+    public T Value()
+    {
+      return instance.Value;
+    }
+  }
+}
