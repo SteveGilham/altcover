2fdc0a02d060583cab0662156cdcabd95a8c938f
Steve Gilham <SteveGilham@users.noreply.github.com>
2021-02-18 11:02:45 +0000

    More generalization

--- AltCover.UICommon/CoverageFile.fs
+++ AltCover.UICommon/CoverageFile.fs
@@ -11,11 +11,27 @@ open System.Xml.Linq
 open System.Xml.Schema
 open System.Xml.XPath
 
-[<SuppressMessage("Microsoft.Design", "CA1027:MarkEnumsWithFlags",
-  Justification="Not used or intended as a flag")>]
-type CoverageTool =
-  | NCoverAlike = 0
-  | OpenCover = 2
+// LCov : TN:  // line-based; branches
+// JSON : {
+// XML : <
+[<NoComparison; AutoSerializable(false)>]
+type CoverageDataType =
+  | Xml
+  | Json
+  | LCov
+  | Unknown
+
+[<NoComparison; AutoSerializable(false)>]
+type XmlCoverageType =
+  | NCover        // <coverage profilerVersion= // have schema .xsd; SeqPnt based
+  | OpenCover     // <CoverageSession xmlns:xsd= // have  schema .xsd; SeqPnt based; branches
+  | Cobertura     // <coverage line-rate= // have schema .dtd; line-based; branches
+//  | DotCover      // <Root CoveredStatements= // seqpnt based
+//// Blue sky time
+//  | PartCover     // <PartCoverReport version= // many versions; seqpnt based
+//  | Dynamic       // <results> // Seqpnt based
+//  | Mprof         // <coverage version="0.3">  // line based
+//  | VisualStudio  // <CoverageDSPriv> // many versions; Seqpnt based
 
 [<NoComparison; AutoSerializable(false)>]
 type InvalidFile =
@@ -49,7 +65,7 @@ module Transformer =
     report
 
   // PartCover to NCover style sheet
-  let internal convertFile (helper : CoverageTool -> XDocument -> XDocument -> XDocument)
+  let internal convertFile (helper : XmlCoverageType -> XDocument -> XDocument -> XDocument)
       (document : XDocument) =
     let schemas = XmlSchemaSet()
     use sr1 = new StreamReader(Assembly.GetExecutingAssembly()
@@ -58,46 +74,42 @@ module Transformer =
     use sr2 = new StreamReader(Assembly.GetExecutingAssembly()
                                        .GetManifestResourceStream("AltCover.UICommon.NCover.xsd"))
 
-// LCov : TN:  // line-based; branches
-// JSON : {
-// XML : <
-
-// XML formats after any preamble (skip <? and <!)
-// + NCover : <coverage profilerVersion= // have schema .xsd; SeqPnt based
-// + OpenCover : <CoverageSession xmlns:xsd= // have  schema .xsd; SeqPnt based; branches
-
-// Cobertura : <coverage line-rate= // have schema .dtd; line-based; branches
-// dotCover : <Root CoveredStatements= // seqpnt based
-
-// Blue sky time
-// PartCover : <PartCoverReport version= // many versions; seqpnt based
-// Dynamic : <results> // Seqpnt based
-// mprof : <coverage version="0.3">  // line based
-// Visual Studio : <CoverageDSPriv> // many versions; Seqpnt based
-
     use ncreader = XmlReader.Create(sr2)
     try
-      match document.XPathSelectElements("/CoverageSession").Count() with
-      | 1 ->
-          schemas.Add
-            (String.Empty, ocreader)
-          |> ignore
-          document.Validate(schemas, null)
-          let report = transformFromOpenCover document
-          let fixedup = helper CoverageTool.OpenCover document report
-          // Consistency check our XSLT
-          let schemas2 = XmlSchemaSet()
-          schemas2.Add
-            (String.Empty, ncreader)
-          |> ignore
-          fixedup.Validate(schemas2, null)
-          Right fixedup
-      | _ ->
-          schemas.Add
-            (String.Empty, ncreader)
-          |> ignore
-          document.Validate(schemas, null)
-          Right document
+      // identify coverage type
+      if document.Root.Name.LocalName = "CoverageSession"
+      then
+        // Assume OpenCover
+        schemas.Add
+          (String.Empty, ocreader)
+        |> ignore
+        document.Validate(schemas, null)
+        let report = transformFromOpenCover document
+        let fixedup = helper XmlCoverageType.OpenCover document report
+        // Consistency check our XSLT
+        let schemas2 = XmlSchemaSet()
+        schemas2.Add
+          (String.Empty, ncreader)
+        |> ignore
+        fixedup.Validate(schemas2, null)
+
+        // Fix for coverlet derived OpenCover (either from coverlet XML or via JsonToXml)
+        let lineOnly = fixedup.Descendants(XName.Get "seqpnt")
+                        |> Seq.forall(fun s -> s.Attribute(XName.Get "line").Value =
+                                                  s.Attribute(XName.Get "endline").Value &&
+                                               s.Attribute(XName.Get "column").Value = "1" &&
+                                               s.Attribute(XName.Get "endcolumn").Value = "2")
+        if lineOnly
+        then fixedup.Root.Add(XAttribute(XName.Get "lineonly", "true"))
+
+        Right fixedup
+      else
+        // Assume NCover
+        schemas.Add
+          (String.Empty, ncreader)
+        |> ignore
+        document.Validate(schemas, null)
+        Right document
     with
     | :? ArgumentNullException as x -> Left(x :> Exception)
     | :? NullReferenceException as x -> Left(x :> Exception)
@@ -106,31 +118,48 @@ module Transformer =
     | :? XmlSchemaValidationException as x -> Left(x :> Exception)
     | :? ArgumentException as x -> Left(x :> Exception)
 
+  let internal firstChar file =
+    use stream = File.OpenRead file
+    use reader = new StreamReader(stream)
+    let chars = Seq.unfold (fun (r:StreamReader) -> let c = r.Read()
+                                                    if c < 0
+                                                    then None
+                                                    else Some (char c, r)) reader
+    chars
+    |> Seq.skipWhile Char.IsWhiteSpace
+    |> Seq.tryHead
+    |> Option.defaultValue '?'
+
+  // simplest possible heuristic
+  let internal identify file =
+    match firstChar file with
+    | '<' -> Xml
+    | '{' -> Json
+    | 'T' -> LCov
+    | _ -> Unknown
+
 [<NoComparison; AutoSerializable(false)>]
 type CoverageFile =
   { File : FileInfo
     Document : XDocument }
 
-  static member ToCoverageFile (helper : CoverageTool -> XDocument -> XDocument -> XDocument)
+  static member ToCoverageFile (helper : XmlCoverageType -> XDocument -> XDocument -> XDocument)
                 (file : FileInfo) =
     try
       let name = file.FullName
-      let rawDocument = if (name |> Path.GetExtension).ToUpperInvariant() = ".JSON"
-                        then name |> NativeJson.fileToJson |> NativeJson.jsonToXml
-                        else XDocument.Load name
+      let filetype = Transformer.identify name
+
+      let rawDocument = match filetype with
+                        | Json -> name |> NativeJson.fileToJson |> NativeJson.jsonToXml
+                        // | Lcov -> // TODO
+                        | _ -> XDocument.Load name  // let invalid files throw XML parse errors
+
       match Transformer.convertFile helper rawDocument with
       | Left x ->
           Left
             { Fault = x
               File = file }
       | Right doc ->
-          let lineOnly = doc.Descendants(XName.Get "seqpnt")
-                         |> Seq.forall(fun s -> s.Attribute(XName.Get "line").Value =
-                                                   s.Attribute(XName.Get "endline").Value &&
-                                                s.Attribute(XName.Get "column").Value = "1" &&
-                                                s.Attribute(XName.Get "endcolumn").Value = "2")
-          if lineOnly
-          then doc.Root.Add(XAttribute(XName.Get "lineonly", "true"))
           Right
             { File = file
               Document = doc }
@@ -173,4 +202,10 @@ module Extensions =
   Target="AltCover.Extensions.#Choice`2.ToOption.Static`2(Microsoft.FSharp.Core.FSharpChoice`2<!!0,!!1>)",
   MessageId="x",
   Justification="Trivial usage")>]
+[<assembly: SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly",
+  Scope="member", Target="AltCover.CoverageDataType+Tags.#LCov", MessageId="Cov",
+  Justification="It is jargon")>]
+[<assembly: SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly",
+  Scope="member", Target="AltCover.XmlCoverageType+Tags.#Cobertura", MessageId="Cobertura",
+  Justification="It is jargon")>]
 ()
