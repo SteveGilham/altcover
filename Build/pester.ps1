914f036b639d9b96360c9852146b5b9f06a993c4
Steve Gilham <SteveGilham@users.noreply.github.com>
2021-02-25 14:39:18 +0000

    Some tidying

--- Build/pester.ps1
+++ Build/pester.ps1
@@ -1,17 +1,12 @@
 param([string]$ACV="0.0.0.0", [string]$ReportName="!:!", [string]$FolderName="!:!")
 
 Import-Module "./_Packaging/$($FolderName)/tools/netcoreapp2.0/AltCover.PowerShell.dll"
-Import-Module "./packages/pester/5.1.1/tools/Pester.psm1"
-
 Invoke-Altcover -?
-# Invoke-Pester -Script @{ Path='.\Build'; Parameters = @{ ACV = $ACV}} -EnableExit -OutputFormat NUnitXml -OutputFile "./_Reports/$($ReportName).xml"
 
+Import-Module "./packages/pester/5.1.1/tools/Pester.psm1"
 $configuration = [PesterConfiguration]::Default
 $configuration.Run.Path = '.\Build'
 $configuration.Run.Exit = $true
-
 $configuration.TestResult.Enabled = $true
-# $configuration.TestResult.OutputFormat = "NUnit2.5"
 $configuration.TestResult.OutputPath = "./_Reports/$($ReportName).xml"
-
 Invoke-Pester -Configuration $configuration
