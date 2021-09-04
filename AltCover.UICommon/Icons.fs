9e7557d2943f45624555713557d5b4ba2293e3b3
Steve Gilham <SteveGilham@users.noreply.github.com>
2021-09-04 20:20:56 +0100

    Fix case-sensitive name + TODO list

--- AltCover.UICommon/Icons.fs
+++ AltCover.UICommon/Icons.fs
@@ -13,27 +13,30 @@ open System.Reflection
 type Icons<'TIcon>(toIcon: System.IO.Stream -> 'TIcon) =
   let makeIcon name =
     lazy
+     try
       (toIcon (
         Assembly
           .GetExecutingAssembly()
           .GetManifestResourceStream("AltCover.UICommon." + name + ".png")
       ))
+     with
+     | x -> System.InvalidOperationException(name, x) |> raise
 
   member self.Xml = makeIcon "XMLFile_16x"
   member self.Assembly = makeIcon "Assembly_16x"
   member self.Event = makeIcon "Event_16x"
   member self.Namespace = makeIcon "Namespace_16x"
   member self.Module = makeIcon "Module_16x"
-  member self.Effect = makeIcon "Effect_16x"
-  member self.Class = makeIcon "class_16x"
+  member self.Effect = makeIcon "Effect_16x" // TODO -- why are exceptions detected as such?
+  member self.Class = makeIcon "Class_16x"
   member self.Property = makeIcon "Property_16x"
   member self.Method = makeIcon "Method_16x"
-  member self.Branched = makeIcon "Branch_12x_16x_grn"
-  member self.Branch = makeIcon "Branch_12x_16x_ylw"
-  member self.RedBranch = makeIcon "Branch_12x_16x_red"
-  member self.Blank = makeIcon "Blank_12x_16x"
+  member self.Branched = makeIcon "Branch_12x_16x_grn" // maybe
+  member self.Branch = makeIcon "Branch_12x_16x_ylw" // maybe + Branch_12x_16x
+  member self.RedBranch = makeIcon "Branch_12x_16x_red" // maybe
+  member self.Blank = makeIcon "Blank_12x_16x" // maybe
   member self.TreeExpand = makeIcon "ExpandRight_16x"
-  member self.TreeCollapse = makeIcon "CollapseChevronDown_16x"
+  member self.TreeCollapse = makeIcon "CollapseChevronDown_16x" // TODO
   member self.MRU = makeIcon "ExpandDown_16x"
   member self.Source = makeIcon "TextFile_16x"
 
@@ -42,9 +45,9 @@ type Icons<'TIcon>(toIcon: System.IO.Stream -> 'TIcon) =
 
   member self.RefreshActive = makeIcon "Refresh_16x"
   member self.Refresh = makeIcon "Refresh_greyThin_16x"
-  member self.Info = makeIcon "StatusInformation_32x"
-  member self.Warn = makeIcon "StatusWarning_32x"
-  member self.Error = makeIcon "StatusCriticalError_32x"
+  member self.Info = makeIcon "StatusInformation_32x" // TODO
+  member self.Warn = makeIcon "StatusWarning_32x" // TODO
+  member self.Error = makeIcon "StatusCriticalError_32x" // TODO
   member self.Open = makeIcon "OpenFile_16x"
   member self.Font = makeIcon "Font_16x"
   member self.ShowAbout = makeIcon "VSTAAbout_16x"
