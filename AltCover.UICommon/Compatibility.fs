9fe24601631d4dfd4c91543a57bc61905bf77e72
Steve Gilham <SteveGilham@users.noreply.github.com>
2021-11-08 21:51:50 +0000

    More gendarme

--- AltCover.UICommon/Compatibility.fs
+++ AltCover.UICommon/Compatibility.fs
@@ -5,6 +5,10 @@ open System.Diagnostics.CodeAnalysis
 open System.Net
 open System.Security.Cryptography
 
+// netstandard 2.0 APIs obsoleted at net 6.0+
+
+[<SuppressMessage("Gendarme.Rules.Smells", "AvoidSpeculativeGeneralityRule",
+  Justification = "It's supposed just to be delegating an API")>]
 [<AutoOpen>]
 module Compatibility =
 #if !GUI
@@ -20,3 +24,12 @@ module Compatibility =
                     Justification = "netstandard 2.0/net472")>]
   let internal createHttp (path:Uri) =
     WebRequest.CreateHttp(path) :> WebRequest
+
+#if GUI
+  [<SuppressMessage("Gendarme.Rules.Maintainability",
+                    "RemoveDependenceOnObsoleteCodeRule",
+                    Justification = "netstandard 2.0/net472")>]
+  let internal readAllText (path:Uri) =
+    use client = new System.Net.WebClient()
+    client.DownloadString(path)
+#endif
