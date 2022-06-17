9d4959556ea600aa21c6260afa7ecffed811c920
Steve Gilham <SteveGilham@users.noreply.github.com>
2022-06-17 19:25:13 +0100

    Supplied test data

---
+++ RegressionTesting/issue156/Class1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using projectNamespace;
+
+namespace Tests
+{
+    [TestClass]
+    public class ConfigurationProviderTest
+    {
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+
+        }
+
+
+        [TestMethod]
+        public void UpdateConfiguration_ConfigurationChangedIsRun()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
