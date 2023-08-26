6ba5fb356e64afa6220ee79596543912e0162fce
Steve Gilham <SteveGilham@users.noreply.github.com>
2023-08-26 19:55:05 +0100

    More

--- AltCover.DotNet/AssemblyInfo.fs
+++ AltCover.DotNet/AssemblyInfo.fs
@@ -4,5 +4,11 @@ open System.Diagnostics.CodeAnalysis
 
 [<assembly: System.CLSCompliant(true)>]
 [<assembly: System.Runtime.InteropServices.ComVisible(false)>]
+[<assembly: SuppressMessage("Microsoft.Performance",
+                            "CA1813:AvoidUnsealedAttributes",
+                            Scope = "type",
+                            Target =
+                              "System.Diagnostics.CodeAnalysis.DynamicDependencyAttribute",
+                            Justification = "Injected type")>]
 
 do ()
