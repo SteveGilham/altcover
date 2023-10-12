4c49a453dac5927f1d1b4adf82cf35a70fd7d716
Steve Gilham <SteveGilham@users.noreply.github.com>
2023-10-12 19:42:59 +0100

    Another maybe

--- AltCover.Monitor.Tests/MonitorTest.fs
+++ AltCover.Monitor.Tests/MonitorTest.fs
@@ -18,7 +18,7 @@ module MonitorTests =
         AltCover.SolutionRoot.location,
         "_Reports/MonitorTestWithAltCoverCoreRunner.net7.0.xml"
       ),
-      [ (260, 37); (260, 36) ] ]
+      [ (260, 37); (260, 36); (226, 36) ] ]
     |> List.filter (fst >> File.Exists)
     |> List.sortBy (fst >> File.GetCreationTimeUtc)
     |> List.last
@@ -82,4 +82,4 @@ module MonitorTests =
     let result = (code, branch)
     let t2 = sprintf "%A" result
 
-    test' <@ List.exists (fun x -> x = result) expect @> (t2 + text)
+    test' <@ List.exists (fun x -> x = result) expect @> (text + t2)
