493e976136e89ab9d40e99735023fe66a863874a
SteveGilham <SteveGilham@users.noreply.github.com>
2024-07-04 13:50:37 +0100

    A bit more reimplementation

--- AltCover.Recorder/Base.cs
+++ AltCover.Recorder/Base.cs
@@ -318,21 +318,38 @@ namespace AltCover.Recorder
     static private class I
 #endif
     {
-      //  let internal openCoverXml =
-      //  ("//Module",
-      //   "hash",
-      //   "Classes/Class/Methods/Method",
-      //   [ ("SequencePoints/SequencePoint", 0)
-      //     ("BranchPoints/BranchPoint", branchFlag) ],
-      //   "vc")
+      internal struct coverXml
+      {
+        internal string m;
+        internal string i;
+        internal string m2;
+        internal Dictionary<string, int> s;
+        internal string v;
+      }
 
-      //let internal IEnumerable<string> nCoverXml =
-      //  ("//module", "moduleId", "method", [ ("seqpnt", 0)], "visitcount")
-      internal static readonly object openCoverXml = "//Module";//TODO
+      internal static readonly coverXml openCoverXml =
+        new coverXml()
+        {
+          m = "//Module",
+          i = "hash",
+          m2 = "Classes/Class/Methods/Method",
+          s = new Dictionary<string, int> { { "SequencePoints/SequencePoint", 0 },
+            { "BranchPoints/BranchPoint", branchFlag }
+          },
+          v = "vc"
+        };
 
-      internal static readonly object nCoverXml = "//module";//TODO
+      internal static readonly coverXml nCoverXml =
+        new coverXml()
+        {
+          m = "//module",
+          i = "moduleId",
+          m2 = "method",
+          s = new Dictionary<string, int> { { "seqpnt", 0 } },
+          v = "visitcount"
+        };
 
-      internal static object xmlByFormat(ReportFormat format)
+      internal static coverXml xmlByFormat(ReportFormat format)
       {
         switch (format & ReportFormat.TrackMask)
         {
@@ -413,6 +430,46 @@ namespace AltCover.Recorder
         return hitcount;
       }
 
+      // TODO inline in release if possible
+      internal static IEnumerable<XmlElement> selectNodes(XmlNode node, string name)
+      {
+        var result = new List<XmlElement>();
+        foreach (var x in node.SelectNodes(name))
+          result.Add(x as XmlElement);
+        return result;
+      }
+
+      // // <summary>
+      // // Load the XDocument
+      // // </summary>
+      // // <param name="path">The XML file to load</param>
+      // // <remarks>Idiom to work with CA2202; we still double dispose the stream, but elude the rule.
+      // // If this is ever a problem, we will need mutability and two streams, with explicit
+      // // stream disposal if and only if the reader or writer doesn't take ownership
+      // // Approved way is ugly -- https://docs.microsoft.com/en-us/visualstudio/code-quality/ca2202?view=vs-2019
+      // // Also, this rule is deprecated
+      // // </remarks>
+      private static XmlDocument readXDocument(Stream stream)
+      {
+        var doc = new XmlDocument();
+
+        try
+        { doc.Load(stream); }
+        catch (XmlException)
+        { doc.LoadXml("<null/>"); }
+
+        return doc;
+      }
+
+      // // <summary>
+      // // Write the XDocument
+      // // </summary>
+      // // <param name="coverageDocument">The XML document to write</param>
+      // // <param name="path">The XML file to write to</param>
+      // // <remarks>Idiom to work with CA2202 as above</remarks>
+      private static void writeXDocument(XmlDocument coverageDocument, Stream stream)
+      { coverageDocument.Save(stream); }
+
       public static DateTime updateReport(
       Action<XmlDocument> postProcess,
       /*Action<XmlElement, IEnumerable<Track>>*/ object pointProcess,
@@ -424,21 +481,129 @@ namespace AltCover.Recorder
       )
       {
         var flushStart = DateTime.UtcNow;
-        //  let flushStart =
-        //    updateReport postProcess pointProcess own counts format coverageFile outputFile
+        var xmlformat = xmlByFormat(format);// throw early on unsupported
+        //let (m, i, m', s, v)
 
-        //  TimeSpan(DateTime.UtcNow.Ticks - flushStart.Ticks)
+        var coverageDocument = readXDocument(coverageFile);
+
+        var root = coverageDocument.DocumentElement;
+
+        var startTimeNode = root.GetAttributeNode("startTime");
+
+        if ((format == ReportFormat.NCover) && !Object.ReferenceEquals(startTimeNode, null))
+        {
+          //then
+          //  let startTimeAttr = startTimeNode.Value
+
+          //  let measureTimeAttr =
+          //    root.GetAttribute("measureTime")
+
+          //  let oldStartTime =
+          //    DateTime.ParseExact(startTimeAttr, "o", null)
+
+          //  let oldMeasureTime =
+          //    DateTime.ParseExact(measureTimeAttr, "o", null)
+
+          //  let st = minTime startTime oldStartTime
+          //  startTime <- st.ToUniversalTime() // Min
+          //  let mt = maxTime measureTime oldMeasureTime
+          //  measureTime <- mt.ToUniversalTime() // Max
+
+          //  root.SetAttribute(
+          //    "startTime",
+          //    startTime.ToString("o", System.Globalization.CultureInfo.InvariantCulture)
+          //  )
+
+          //  root.SetAttribute(
+          //    "measureTime",
+          //    measureTime.ToString("o", System.Globalization.CultureInfo.InvariantCulture)
+          //  )
+
+          //  root.SetAttribute(
+          //    "driverVersion",
+          //    "AltCover.Recorder "
+          //    + System.Diagnostics.FileVersionInfo
+          //      .GetVersionInfo(
+          //        System.Reflection.Assembly
+          //          .GetExecutingAssembly()
+          //          .Location
+          //      )
+          //      .FileVersion
+          //  )
+        }
+
+        var moduleNodes = selectNodes(coverageDocument, xmlformat.m);
+
+        //moduleNodes
+        //|> Seq.cast<XmlElement>
+        //|> Seq.map (fun el -> el.GetAttribute(i), el)
+        //|> Seq.filter (fun (k, _) -> counts.ContainsKey k)
+        //|> Seq.iter (fun (k, affectedModule) ->
+        //  let moduleHits = counts.[k]
+        //  // Don't do this in one leap like --
+        //  // affectedModule.Descendants(XName.Get("seqpnt"))
+        //  // Get the methods, then flip their
+        //  // contents before concatenating
+        //  let nn = selectNodes affectedModule m'
+
+        //  nn
+        //  |> Seq.cast<XmlElement>
+        //  |> Seq.collect (fun (method: XmlElement) ->
+        //    s
+        //    |> Seq.collect (fun (name, flag) ->
+        //      let nodes = selectNodes method name
+
+        //      nodes
+        //      |> Seq.cast<XmlElement>
+        //      |> Seq.map (fun x -> (x, flag))
+        //      |> Seq.toList
+        //      |> List.rev))
+        //  |> Seq.mapi (fun counter (pt, flag) ->
+        //    ((match format &&& ReportFormat.TrackMask with
+        //      | ReportFormat.OpenCover ->
+        //        "uspid"
+        //        |> pt.GetAttribute
+        //        |> (findIndexFromUspid flag)
+        //      | _ -> counter),
+        //     pt))
+        //  |> Seq.filter (fst >> moduleHits.ContainsKey)
+        //  |> Seq.iter (fun x ->
+        //    let pt = snd x
+        //    let counter = fst x
+
+        //    let vc =
+        //      Int64.TryParse(
+        //        pt.GetAttribute(v),
+        //        System.Globalization.NumberStyles.Integer,
+        //        System.Globalization.CultureInfo.InvariantCulture
+        //      )
+        //      |> snd
+        //    // Treat -ve visit counts (an exemption added in analysis) as zero
+        //    let count = moduleHits.[counter]
+        //    let visits = (max 0L vc) + count.Total
+        //    pt.SetAttribute(v, visits.ToString(CultureInfo.InvariantCulture))
+        //    pointProcess pt count.Tracks))
+
+        postProcess(coverageDocument);
+
+        // Save modified xml to a file
+        outputFile.Seek(0L, SeekOrigin.Begin);
+        outputFile.SetLength(0);
+
+        if (own)
+        { writeXDocument(coverageDocument, outputFile); }
+
         return flushStart;
       }
 
       public static TimeSpan doFlush(
-      Action<XmlDocument> postProcess,
+        Action<XmlDocument> postProcess,
       /*Action<XmlElement, IEnumerable<Track>>*/ object pointProcess,
-      bool own,
-      Dictionary<string, Dictionary<int, PointVisit>> counts,
-      ReportFormat format,
-      Stream coverageFile,
-      Stream outputFile
+        bool own,
+        Dictionary<string, Dictionary<int, PointVisit>> counts,
+        ReportFormat format,
+        Stream coverageFile,
+        Stream outputFile
       )
       {
         var flushStart =
