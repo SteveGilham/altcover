8020b2bfbd8160cdf1be55c19d551852bb073478
SteveGilham <SteveGilham@users.noreply.github.com>
2024-07-12 17:56:38 +0100

    Revert to F# tests, just not built at net20 - breaks as API isn't there yet

--- AltCover.Recorder/Recorder.cs
+++ AltCover.Recorder/Recorder.cs
@@ -11,11 +11,32 @@ namespace AltCover.Recorder
   using System.Runtime.CompilerServices;
 
 #if !NET20
-using System.Threading;
+  using System.Threading;
 #endif
 
   //using AltCover.Shared;
 
   public static class Instance
-  { }
+  {
+    // Public "fields"
+
+    /// <summary>
+    /// Gets the location of coverage xml file
+    /// This property's IL code is modified to store the actual file location
+    /// </summary>
+    public static string ReportFile
+    {
+      [MethodImpl(MethodImplOptions.NoInlining)]
+      get { return "Coverage.Default.xml"; }
+    }
+
+#if DEBUG
+
+    internal static class I
+#else
+     private static class I
+#endif
+    {
+    }
+  }
 }
