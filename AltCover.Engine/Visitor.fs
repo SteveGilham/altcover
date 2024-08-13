b17d85d97b655edd665f4d20a494e91edb95a87f
SteveGilham <SteveGilham@users.noreply.github.com>
2024-08-13 13:45:08 +0100

    Refactor using the slightly less obvious API

--- AltCover.Engine/Visitor.fs
+++ AltCover.Engine/Visitor.fs
@@ -1193,12 +1193,12 @@ module internal Visitor =
           null
       | _ -> seq
 
-    let getSequencePoint (dbg: IDictionary<int, SequencePoint>) (i: Instruction) =
-      dbg.TryGetValue(i.Offset) |> snd
+    let getSequencePoint (dbg: IDictionary<Instruction, SequencePoint>) (i: Instruction) =
+      i |> dbg.TryGetValue |> snd
 
     let internal findEffectiveSequencePoint
       genuine
-      (dbg: Dictionary<int, SequencePoint>)
+      (dbg: IDictionary<Instruction, SequencePoint>)
       (instructions: Instruction seq)
       =
       instructions
@@ -1208,7 +1208,7 @@ module internal Visitor =
       |> Seq.tryFind isSequencePoint
 
     let internal findSequencePoint
-      (dbg: Dictionary<int, SequencePoint>)
+      (dbg: IDictionary<Instruction, SequencePoint>)
       (instructions: Instruction seq)
       =
       findEffectiveSequencePoint Genuine dbg instructions
@@ -1276,7 +1276,7 @@ module internal Visitor =
 
     [<TailCall>]
     let rec internal lastOfSequencePoint
-      (dbg: IDictionary<int, SequencePoint>)
+      (dbg: IDictionary<Instruction, SequencePoint>)
       (i: Instruction)
       =
       let n = i.Next
@@ -1291,7 +1291,7 @@ module internal Visitor =
 
     [<TailCall>]
     let rec internal firstOfSequencePoint
-      (dbg: IDictionary<int, SequencePoint>)
+      (dbg: IDictionary<Instruction, SequencePoint>)
       (i: Instruction)
       =
       let p = i.Previous
@@ -1304,7 +1304,10 @@ module internal Visitor =
       else
         firstOfSequencePoint dbg p
 
-    let internal getJumps (dbg: Dictionary<int, SequencePoint>) (i: Instruction) =
+    let internal getJumps
+      (dbg: IDictionary<Instruction, SequencePoint>)
+      (i: Instruction)
+      =
       let terminal = lastOfSequencePoint dbg i
       let next = i.Next
 
@@ -1404,7 +1407,7 @@ module internal Visitor =
 
     let private extractBranchPoints
       (v0t: TypeReference option)
-      (dbg: Dictionary<int, SequencePoint>)
+      (dbg: IDictionary<Instruction, SequencePoint>)
       rawInstructions
       interesting
       vc
@@ -1481,15 +1484,15 @@ module internal Visitor =
             Key = i }))
       |> Seq.choose id
       |> processBranches
-      |> Seq.map BranchPoint
+      |> Seq.map BranchPoint // AltCover.Visitor/I/Pipe #2 stage #10
       |> Seq.toList
 
     let internal validateInstruction
-      (dbg: IDictionary<int, SequencePoint>)
+      (dbg: IDictionary<Instruction, SequencePoint>)
       (x: Instruction)
       =
-      let (_, s) = dbg.TryGetValue x.Offset
-      s.IsNotNull && (s.IsHidden |> not)
+      let (yes, s) = dbg.TryGetValue x
+      yes && (s.IsHidden |> not)
 
     let internal trivial =
       HashSet(
@@ -1502,21 +1505,14 @@ module internal Visitor =
       )
 
     let internal isNonTrivialSeqPnt
-      (dbg: Dictionary<int, SequencePoint>)
+      (dbg: IDictionary<Instruction, SequencePoint>)
       (x: Instruction)
       =
       if CoverageParameters.trivia.Value then
         let rest = // rest of the sequence point
           Seq.unfold
             (fun (i: Instruction) ->
-              if
-                i |> isNull
-                || i.Offset
-                   |> dbg.TryGetValue
-                   |> snd
-                   |> isNull
-                   |> not
-              then
+              if i |> isNull || i |> dbg.ContainsKey then
                 None
               else
                 Some(i, i.Next))
@@ -1536,16 +1532,14 @@ module internal Visitor =
 
       let rawInstructions = body.Instructions
 
-      let splut = Dictionary<int, SequencePoint>()
-
-      do
-        let dbg = m.Method.DebugInformation
-
-        if dbg.IsNotNull && dbg.HasSequencePoints then
-          dbg.SequencePoints
-          |> Seq.iter (fun s -> splut.Add(s.Offset, s))
-
-      // build more look-up tables
+      let splut =
+        m.Method.DebugInformation
+        |> Option.ofObj
+        |> Option.map _.GetSequencePointMapping()
+        |> Option.defaultValue (
+          Dictionary<Instruction, SequencePoint>()
+          :> IDictionary<Instruction, SequencePoint>
+        )
 
       let instructions =
         [ rawInstructions |> Seq.cast ]
@@ -1581,7 +1575,7 @@ module internal Visitor =
             MethodPoint
               { Instruction = i
                 SeqPnt =
-                  splut.TryGetValue(i.Offset)
+                  splut.TryGetValue(i)
                   |> snd
                   |> Option.ofObj
                   |> Option.filter (fun _ -> CoverageParameters.methodPoint.Value)
@@ -1592,11 +1586,11 @@ module internal Visitor =
         else
           instructions.OrderByDescending(fun (x: Instruction) -> x.Offset)
           |> Seq.mapi (fun i x ->
-            let s = splut.TryGetValue(x.Offset) |> snd
+            let s = x |> splut.TryGetValue |> snd
 
             MethodPoint
               { Instruction = x
-                SeqPnt = s |> SeqPnt.Build |> Some
+                SeqPnt = s |> SeqPnt.Build |> Some // AltCover.Visitor/I/sp@1
                 Uid = i + point
                 Interesting = wanted interesting s
                 DefaultVisitCount = m.DefaultVisitCount })
@@ -1611,7 +1605,7 @@ module internal Visitor =
       let bp =
         if includeBranches () then
           let spnt =
-            (instructions |> Seq.head).Offset
+            (instructions |> Seq.head)
             |> splut.TryGetValue
             |> snd
 
@@ -1739,12 +1733,12 @@ module internal Visitor =
                             "UseCorrectCasingRule",
                             Scope = "member", // MethodDefinition
                             Target =
-                              "AltCover.Visitor/I/sp@1599-2::Invoke(AltCover.SeqPnt)",
+                              "AltCover.Visitor/I/sp@1593-2::Invoke(AltCover.SeqPnt)",
                             Justification = "Inlined library code")>]
 [<assembly: SuppressMessage("Gendarme.Rules.Naming",
                             "UseCorrectCasingRule",
                             Scope = "member", // MethodDefinition
                             Target =
-                              "AltCover.Visitor/I/Pipe #2 stage #10 at line 1484@1484::Invoke(AltCover.GoTo)",
+                              "AltCover.Visitor/I/Pipe #2 stage #10 at line 1487@1487::Invoke(AltCover.GoTo)",
                             Justification = "Inlined library code")>]
 ()
