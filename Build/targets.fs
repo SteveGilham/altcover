f1b4824cf260bf712785aa3b12e68c41b20337e7
Steve Gilham <SteveGilham@users.noreply.github.com>
2025-01-19 22:01:27 +0100

    Try that again, properly

--- Build/targets.fs
+++ Build/targets.fs
@@ -5779,21 +5779,44 @@ module Targets =
           Path.GetDirectoryName test
         | _ -> Path.getFullName "_Packaging/Unpack/tools/net472"
 
+      // MSBuild.build
+      //   (fun p ->
+      //     { p with
+      //         Verbosity = Some MSBuildVerbosity.Minimal
+      //         ConsoleLogParameters = []
+      //         DistributedLoggers = None
+      //         DisableInternalBinLog = true
+      //         Properties =
+      //           [ "Configuration", "Debug"
+      //             "MSBuildTest", "true"
+      //             "AltCoverTag", "MSBuildTest_"
+      //             "AltCoverPath", unpack.Replace('\\', '/')
+      //             "DebugSymbols", "True"
+      //             "Fake2738Workround", "True" ] })
+      //   "./Samples/Sample4/Sample4LongForm.fsproj")
+
       DotNet.msbuild
-        (fun p ->
-          { p with
-              Verbosity = Some MSBuildVerbosity.Minimal
-              ConsoleLogParameters = []
-              DistributedLoggers = None
-              DisableInternalBinLog = true
-              Properties =
+        (fun opt ->
+          let tmp =
+            opt.WithCommon(fun o' ->
+              { dotnetOptions o' with
+                  WorkingDirectory = sample })
+
+          { tmp with
+              Fake.DotNet.DotNet.MSBuildOptions.MSBuildParams.ConsoleLogParameters = []
+              Fake.DotNet.DotNet.MSBuildOptions.MSBuildParams.DistributedLoggers = None
+              Fake.DotNet.DotNet.MSBuildOptions.MSBuildParams.DisableInternalBinLog =
+                true
+              Fake.DotNet.DotNet.MSBuildOptions.MSBuildParams.Properties =
                 [ "Configuration", "Debug"
                   "MSBuildTest", "true"
                   "AltCoverTag", "MSBuildTest_"
                   "AltCoverPath", unpack.Replace('\\', '/')
                   "DebugSymbols", "True"
-                  "Fake2738Workround", "True" ] })
+                  "Fake2738Workround", "True" ] 
+                @ tmp.MSBuildParams.Properties })
         "./Samples/Sample4/Sample4LongForm.fsproj")
+
 
   let cake5plusTest =
     (fun () ->
