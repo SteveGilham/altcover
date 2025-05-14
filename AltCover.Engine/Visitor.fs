95bebed6ef29b4470bd12a30d0bc471d67742273
SteveGilham <SteveGilham@users.noreply.github.com>
2025-05-14 14:41:25 +0100

    Bump dotnet SDK version (and other dependencies)

--- AltCover.Engine/Visitor.fs
+++ AltCover.Engine/Visitor.fs
@@ -478,6 +478,9 @@ module internal CoverageParameters =
   [<SuppressMessage("Gendarme.Rules.Portability",
                     "NewLineLiteralRule",
                     Justification = "Constant string exactly for portability")>]
+  [<SuppressMessage("Gendarme.Rules.Performance",
+                    "AvoidLargeNumberOfLocalVariablesRule",
+                    Justification = "Compiler change at dotnet 9.0.300")>]
   let internal makeConfiguration () =
     let components =
       [ "--methodpoint\t" + string methodPoint.Value
@@ -1721,18 +1724,18 @@ module internal Visitor =
                             "UseCorrectCasingRule",
                             Scope = "member", // MethodDefinition
                             Target =
-                              "AltCover.Visitor/I/sp@1581-2::Invoke(AltCover.SeqPnt)",
+                              "AltCover.Visitor/I/sp@1584-2::Invoke(AltCover.SeqPnt)",
                             Justification = "Inlined library code")>]
 [<assembly: SuppressMessage("Gendarme.Rules.Naming",
                             "UseCorrectCasingRule",
                             Scope = "member", // MethodDefinition
                             Target =
-                              "AltCover.Visitor/I/Pipe #2 stage #10 at line 1474@1474::Invoke(AltCover.GoTo)",
+                              "AltCover.Visitor/I/Pipe #2 stage #10 at line 1477@1477::Invoke(AltCover.GoTo)",
                             Justification = "Inlined library code")>]
 [<assembly: SuppressMessage("Gendarme.Rules.Exceptions",
                             "InstantiateArgumentExceptionCorrectlyRule",
                             Scope = "member", // MethodDefinition
                             Target =
-                              "AltCover.Visitor/I/finish@1253::Invoke(Microsoft.FSharp.Core.FSharpFunc`2<Mono.Cecil.Cil.Instruction,System.Int32>,Microsoft.FSharp.Collections.FSharpList`1<Mono.Cecil.Cil.Instruction>)",
+                              "AltCover.Visitor/I/finish@1256::Invoke(Microsoft.FSharp.Core.FSharpFunc`2<Mono.Cecil.Cil.Instruction,System.Int32>,Microsoft.FSharp.Collections.FSharpList`1<Mono.Cecil.Cil.Instruction>)",
                             Justification = "Inlined library code")>]
 ()
